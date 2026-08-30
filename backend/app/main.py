38048bc2673efdfca29befcb38a19da8284f2904
panxo <franciscovillalobos025@gmail.com>
2026-08-29 20:23:08 -0400

    jwt funcionando

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.user_route import router as user_router
 from app.routes.auth_route import router as auth_router
 
@@ -24,6 +25,18 @@ app = FastAPI(
     version="1.0.0",
     lifespan=lifespan
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router)
 app.include_router(auth_router)
 
